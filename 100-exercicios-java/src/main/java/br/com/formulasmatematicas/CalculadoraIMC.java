4d0559367303b4701b42239eb556bce3edfa2fd3
GeorgeSalu <george.salu10@gmail.com>
2023-10-13 19:16:18 -0300

    100-exercicios-java - exercicio 5

---
+++ 100-exercicios-java/src/main/java/br/com/formulasmatematicas/CalculadoraIMC.java
@@ -0,0 +1,26 @@
+package br.com.formulasmatematicas;
+
+import java.util.Scanner;
+
+/*
+ * Escreva um programa que calcule o IMC de um individuo
+ * utilizando a formula IMC = peso / altura  
+ * 
+ * */
+public class CalculadoraIMC {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite o peso em quilogramas : ");
+		double peso = scanner.nextDouble();
+		
+		System.out.println("Digite a altura em metros : ");
+		double altura = scanner.nextDouble();
+		
+		double imc = peso / (altura * altura);
+		
+		System.out.println("O IMC é : "+imc);
+		
+		scanner.close();
+	}
+}
