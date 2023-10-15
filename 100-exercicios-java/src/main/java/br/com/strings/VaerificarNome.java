93ab3832b6aeafbceb4d127a37feb710742089d7
GeorgeSalu <george.salu10@gmail.com>
2023-10-15 12:14:57 -0300

    100-exercicios-java - strings - exercicio 19

---
+++ 100-exercicios-java/src/main/java/br/com/strings/VaerificarNome.java
@@ -0,0 +1,29 @@
+package br.com.strings;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * Escreva um programa que receba um nome e 
+ * verifique se o mesmo começa com a letra 'A'
+ *
+ */
+public class VaerificarNome {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite um nome : ");
+		String nome = scanner.nextLine();
+		
+		// convertendo o nome para letras minusculas para facilitar a comparacao
+		String nomeMinusculo = nome.toLowerCase();
+		
+		if(nomeMinusculo.startsWith("a")) {
+			System.out.println("O nome comça com a letra A");
+		} else {
+			System.out.println("O nome não começa com a letra A");
+		}
+		
+		scanner.close();
+	}
+}
