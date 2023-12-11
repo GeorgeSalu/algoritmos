fce281e55f1a94e76cf618585721c27110f61d3e
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 10:17:33 -0300

    100-exercicios-java - strings - exercicio 23

---
+++ 100-exercicios-java/src/main/java/br/com/strings/ContarEspacos.java
@@ -0,0 +1,11 @@
+package br.com.strings;
+
+/**
+ * 
+ * Faça um programa que receba uma frase e exiba a 
+ * quantidade de espaços em branco presentes na mesma
+ *
+ */
+public class ContarEspacos {
+
+}
