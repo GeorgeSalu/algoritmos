92e059c6ca83fed6352ca83a95a6c3ee1752135a
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 20:46:58 -0300

    100-exercicios-java - condicionais - solucao exercicio MaiorNumero

--- 100-exercicios-java/src/main/java/br/com/condicionais/MaiorNumero.java
+++ 100-exercicios-java/src/main/java/br/com/condicionais/MaiorNumero.java
@@ -1,5 +1,7 @@
 package br.com.condicionais;
 
+import java.util.Scanner;
+
 /***
  * 
  * escreva um programa que solicite tres 
@@ -7,5 +9,30 @@ package br.com.condicionais;
  *
  */
 public class MaiorNumero {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("digite o primeiro numero : ");
+		int numero1 = scanner.nextInt();
+		
+		System.out.println("digite o segundo numero : ");
+		int numero2 = scanner.nextInt();
+		
+		System.out.println("digite o terceiro numero : ");
+		int numero3 = scanner.nextInt();
+		
+		int maior = numero1;
+		
+		if(numero2 > maior) {
+			maior = numero2;
+		}
+		
+		if(numero3 > maior) {
+			maior = numero3;
+		}
+		
+		System.out.println("o maior numero é : "+maior);
+		
+		scanner.close();
+	}
 }
