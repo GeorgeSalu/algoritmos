cb1024295f822e84412ef18ea64df6ef3c0e427b
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 20:52:01 -0300

    100-exercicios-java - condicionais - solucao exercicio VerificarNumero

--- 100-exercicios-java/src/main/java/br/com/condicionais/VerificarNumero.java
+++ 100-exercicios-java/src/main/java/br/com/condicionais/VerificarNumero.java
@@ -1,5 +1,7 @@
 package br.com.condicionais;
 
+import java.util.Scanner;
+
 /***
  * 
  * Faça um programa que leia um numero e
@@ -7,5 +9,20 @@ package br.com.condicionais;
  *
  */
 public class VerificarNumero {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("digite um numero : ");
+		int numero = scanner.nextInt();
+		
+		if(numero > 0) {
+			System.out.println("o numero é positivo");
+		} else if(numero < 0) {
+			System.out.println("o numero é negativo");
+		} else {
+			System.out.println("o umero é zero");
+		}
+		
+		scanner.close();
+	}
 }
