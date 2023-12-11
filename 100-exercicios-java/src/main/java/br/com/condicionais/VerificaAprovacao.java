55ea248294bfee0df4eb43ef1d960d2b6c8fd488
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 20:56:10 -0300

    100-exercicios-java - condicionais - exercicio VerificarAprovacao

---
+++ 100-exercicios-java/src/main/java/br/com/condicionais/VerificaAprovacao.java
@@ -0,0 +1,12 @@
+package br.com.condicionais;
+
+/***
+ * 
+ * Faça um programa que leia as notas de duas provas
+ * e informe se o aluno foi aprovado (nota maior ou igual 6)
+ * ou reprovado (nota menor que 6) em casa umas das provas
+ *
+ */
+public class VerificaAprovacao {
+
+}
