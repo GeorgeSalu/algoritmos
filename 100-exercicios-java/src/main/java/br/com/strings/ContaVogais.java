4b5fd2c116b275963c8ab43dcac93547e78b302e
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 10:51:30 -0300

    100-exercicios-java - strings - solucao exercicio 24

--- 100-exercicios-java/src/main/java/br/com/strings/ContaVogais.java
+++ 100-exercicios-java/src/main/java/br/com/strings/ContaVogais.java
@@ -1,5 +1,7 @@
 package br.com.strings;
 
+import java.util.Scanner;
+
 /**
  * 
  * crie um programa que leia uma palavra e exiba 
@@ -7,5 +9,25 @@ package br.com.strings;
  *
  */
 public class ContaVogais {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite uma palavra : ");
+		String palavra = scanner.nextLine();
+		
+		int contadorVogais = 0;
+		
+		// percorre cada caractere da palavra
+		for(int i = 0;i < palavra.length(); i++) {
+			char letra = Character.toLowerCase(palavra.charAt(i));
+			
+			// verifica se a letra é uma vogal
+			if(letra == 'a' | letra == 'e' | letra == 'i' | letra == 'o' | letra == 'u' ) {
+				contadorVogais++;
+			}
+		}
+		
+		System.out.println("quantidade de vogais "+contadorVogais);
+		scanner.close();
+	}
 }
