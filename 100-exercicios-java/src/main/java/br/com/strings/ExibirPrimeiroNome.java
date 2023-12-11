90eff9619dd75a1787015a44fc3409ac6700363d
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 10:15:50 -0300

    100-exercicios-java - strings - solucao exercicio 22

--- 100-exercicios-java/src/main/java/br/com/strings/ExibirPrimeiroNome.java
+++ 100-exercicios-java/src/main/java/br/com/strings/ExibirPrimeiroNome.java
@@ -1,5 +1,7 @@
 package br.com.strings;
 
+import java.util.Scanner;
+
 /**
  * 
  * Escreva um programa que receba um nome 
@@ -7,5 +9,20 @@ package br.com.strings;
  *
  */
 public class ExibirPrimeiroNome {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite o nome completo: ");
+		String nomeCompleto = scanner.nextLine();
+		
+		// dividir o nome completo em partes separadas pelo espaco em branco
+		String[] partesNome = nomeCompleto.split(" ");
+		
+		// obter o primeiro nome
+		String primieroNome = partesNome[0];
+		
+		System.out.println("primeiro nome : "+primieroNome);
+		
+		scanner.close();
+	}
 }
