930c0ee0d03f75e864e4daecda2168d873f8d6da
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 10:23:05 -0300

    100-exercicios-java - strings - solucao exercicio 23

--- 100-exercicios-java/src/main/java/br/com/strings/ContarEspacos.java
+++ 100-exercicios-java/src/main/java/br/com/strings/ContarEspacos.java
@@ -1,5 +1,8 @@
 package br.com.strings;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
 /**
  * 
  * Faça um programa que receba uma frase e exiba a 
@@ -7,5 +10,24 @@ package br.com.strings;
  *
  */
 public class ContarEspacos {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite uma frase : ");
+		String frase = scanner.nextLine();
+		
+		int contadorEspacos = 0;
+		
+		// percorre cada caractere da frase
+		for(int i = 0;i < frase.length(); i++) {
+			// verifica se o caracter atual é um espaco em branco
+			if(frase.charAt(i) == ' ') {
+				contadorEspacos++;
+			}
+		}
+		
+		System.out.println("a quantidade de espaços em branco : "+contadorEspacos);
+		
+		scanner.close();
+	}
 }
