1824fb6575fda79f8d892e72d9470ab2980ef20c
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 10:56:59 -0300

    100-exercicios-java - strings - solucao exercicio 25

--- 100-exercicios-java/src/main/java/br/com/strings/SobrenomePrimeiro.java
+++ 100-exercicios-java/src/main/java/br/com/strings/SobrenomePrimeiro.java
@@ -1,5 +1,7 @@
 package br.com.strings;
 
+import java.util.Scanner;
+
 /**
  * 
  * escreva um programa que receba um nome completo
@@ -7,5 +9,23 @@ package br.com.strings;
  *
  */
 public class SobrenomePrimeiro {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite um nome completo : ");
+		String nomeCompleto = scanner.nextLine();
+		
+		String[] partes = nomeCompleto.split(" ");
+		String sobrenome = partes[partes.length - 1];
+		
+		System.out.println("nome com sobrenome primeiro");
+		System.out.println(sobrenome);
+		
+		for(int i = 0; i < partes.length - 1;i++) {
+			System.out.println(" "+partes[i]);
+		}
+		System.out.println("");
+		
+		scanner.close();
+	}
 }
