6eac376de9b24d7b5683e867ed027abd9fddcfa4
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 09:43:22 -0300

    100-exercicios-java - strings - exercicio 19

---
+++ 100-exercicios-java/src/main/java/br/com/strings/VerificarPalindromo.java
@@ -0,0 +1,13 @@
+package br.com.strings;
+
+/**
+ * 
+ * @author george
+ * 
+ * Faça um programa que leia uma palavra e verifique se a mesma
+ * é um palindromo (se pode ser lida da mesma forma de trás pra frente)
+ *
+ */
+public class VerificarPalindromo {
+
+}
