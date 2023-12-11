cfa4b83da35b6f4eba66d654c0c6253d27b2bc54
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 20:59:04 -0300

    100-exercicios-java - condicionais - solucao exercicio VerificarAprovacao

--- 100-exercicios-java/src/main/java/br/com/condicionais/VerificaAprovacao.java
+++ 100-exercicios-java/src/main/java/br/com/condicionais/VerificaAprovacao.java
@@ -1,5 +1,7 @@
 package br.com.condicionais;
 
+import java.util.Scanner;
+
 /***
  * 
  * Faça um programa que leia as notas de duas provas
@@ -8,5 +10,27 @@ package br.com.condicionais;
  *
  */
 public class VerificaAprovacao {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("digite a nota da primeira prova : ");
+		double nota1 = scanner.nextDouble();
+		
+		System.out.println("digite a nota da segunda prova : ");
+		double nota2 = scanner.nextDouble();
+		
+		if(nota1 >= 6) {
+			System.out.println("aluno aprovado na primeira prova");
+		} else {
+			System.out.println("aluno reprovado na primeira prova");
+		}
+		
+		if(nota2 >= 6) {
+			System.out.println("aluno aprovado na segunda prova");
+		} else {
+			System.out.println("aluno reprovado na segunda prova");
+		}
+		
+		scanner.close();
+	}
 }
