70e13ae316d04172dbf82a34157ff3cbecbf7524
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 10:53:00 -0300

    100-exercicios-java - strings - exercicio 25

---
+++ 100-exercicios-java/src/main/java/br/com/strings/SobrenomePrimeiro.java
@@ -0,0 +1,11 @@
+package br.com.strings;
+
+/**
+ * 
+ * escreva um programa que receba um nome completo
+ * e exiba o sobrenome(ultimo nome) primeiro
+ *
+ */
+public class SobrenomePrimeiro {
+
+}
