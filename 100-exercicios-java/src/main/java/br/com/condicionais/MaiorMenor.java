2325ab713e3f956ca7e2fc1a2512d45b1c3ba653
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 20:41:56 -0300

    100-exercicios-java - condicionais - solucao exercicio 27

--- 100-exercicios-java/src/main/java/br/com/condicionais/MaiorMenor.java
+++ 100-exercicios-java/src/main/java/br/com/condicionais/MaiorMenor.java
@@ -1,5 +1,7 @@
 package br.com.condicionais;
 
+import java.util.Scanner;
+
 /***
  * 
  * Faça um programa que leia dois 
@@ -7,5 +9,23 @@ package br.com.condicionais;
  *
  */
 public class MaiorMenor {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("digite o primeiro numero : ");
+		int numero1 = scanner.nextInt();
+		
+		System.out.println("digite o segundo numero : ");
+		int numero2 = scanner.nextInt();
+		
+		if(numero1 > numero2) {
+			System.out.println("o primeiro numero é maior");
+		} else if(numero2 > numero1) {
+			System.out.println("o segundo numero pe maior");
+		} else {
+			System.out.println("os numeros são iguais");
+		}
+		
+		scanner.close();
+	}
 }
