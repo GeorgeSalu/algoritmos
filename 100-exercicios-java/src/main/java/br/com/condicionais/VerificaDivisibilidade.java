67972d5ef626d673add3a37265a286168e24605c
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 21:06:05 -0300

    100-exercicios-java - condicionais - exercicio VerificarDivisibilidade

---
+++ 100-exercicios-java/src/main/java/br/com/condicionais/VerificaDivisibilidade.java
@@ -0,0 +1,11 @@
+package br.com.condicionais;
+
+/***
+ * 
+ * Faça um programa que leia tres numeros, e 
+ * informe se a soma deles e divisivel por 5 ou não
+ *
+ */
+public class VerificaDivisibilidade {
+
+}
