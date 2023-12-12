d19580fadfb935fa8801ce03dfa291a96bf8591c
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 21:03:52 -0300

    100-exercicios-java - condicionais - solucao exercicio VerificarAprovacaMediao

--- 100-exercicios-java/src/main/java/br/com/condicionais/VerificaAprovacaoMedia.java
+++ 100-exercicios-java/src/main/java/br/com/condicionais/VerificaAprovacaoMedia.java
@@ -1,5 +1,7 @@
 package br.com.condicionais;
 
+import java.util.Scanner;
+
 /***
  * 
  * Faça um programa que leia as notas de duas provas, calcule
@@ -8,5 +10,25 @@ package br.com.condicionais;
  *
  */
 public class VerificaAprovacaoMedia {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("digite a nota da primeira prova : ");
+		double nota1 = scanner.nextDouble();
+		
+		System.out.println("digite a nota da segunda prova : ");
+		double nota2 = scanner.nextDouble();
+		
+		double media = (nota1 + nota2) / 2;
+		
+		System.out.println("media : "+media);
+		
+		if(media >= 6) {
+			System.out.println("aluno aprovado");
+		} else {
+			System.out.println("aluno reprovado");
+		}
+		
+		scanner.close();
+	}
 }
