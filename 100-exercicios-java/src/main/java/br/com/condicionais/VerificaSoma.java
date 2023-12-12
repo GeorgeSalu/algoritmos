575d9016ea9a0d72831aac0c330b92cc8bc1a1f1
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 21:17:25 -0300

    100-exercicios-java - condicionais - exercicio VerificarSoma

---
+++ 100-exercicios-java/src/main/java/br/com/condicionais/VerificaSoma.java
@@ -0,0 +1,11 @@
+package br.com.condicionais;
+
+/***
+ * 
+ * crie um programa que leia tres numeros e verifique 
+ * se a soma deles é positiva, negativa ou igual a zero
+ *
+ */
+public class VerificaSoma {
+
+}
