2cb4685f5d9a03b73f243364f411ea1cde4011e8
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 21:00:49 -0300

    100-exercicios-java - condicionais - exercicio VerificarAprovacaMediao

---
+++ 100-exercicios-java/src/main/java/br/com/condicionais/VerificaAprovacaoMedia.java
@@ -0,0 +1,12 @@
+package br.com.condicionais;
+
+/***
+ * 
+ * Faça um programa que leia as notas de duas provas, calcule
+ * a media aritmetica simples, e informe se o aluno foi aprovado
+ * (media maior ou igual a 6) ou reprovado (media menor que 6)
+ *
+ */
+public class VerificaAprovacaoMedia {
+
+}
