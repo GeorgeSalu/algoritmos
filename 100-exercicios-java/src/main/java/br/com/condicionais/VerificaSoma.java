bba96344fed42d250d538afccb25118288226f3b
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 21:20:46 -0300

    100-exercicios-java - condicionais - solucao exercicio VerificarSoma

--- 100-exercicios-java/src/main/java/br/com/condicionais/VerificaSoma.java
+++ 100-exercicios-java/src/main/java/br/com/condicionais/VerificaSoma.java
@@ -1,5 +1,7 @@
 package br.com.condicionais;
 
+import java.util.Scanner;
+
 /***
  * 
  * crie um programa que leia tres numeros e verifique 
@@ -7,5 +9,28 @@ package br.com.condicionais;
  *
  */
 public class VerificaSoma {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("digite o primeiro numero : ");
+		int numero1 = scanner.nextInt();
+		
+		System.out.println("digite o segundo numero : ");
+		int numero2 = scanner.nextInt();
+		
+		System.out.println("digite o terceiro numero : ");
+		int numero3 = scanner.nextInt();
+		
+		int soma = numero1 + numero2 + numero3;
+		
+		if(soma > 0) {
+			System.out.println("a soma pe positiva");
+		} else if(soma < 0) {
+			System.out.println("a soma é negativa");
+		} else {
+			System.out.println("a soma e igual a zero");
+		}
+		
+		scanner.close();
+	}
 }
