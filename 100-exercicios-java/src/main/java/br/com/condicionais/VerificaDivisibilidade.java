1040ccf110c199f5788b35ed691de5b7bb91fc3e
GeorgeSalu <george.salu10@gmail.com>
2023-12-11 21:16:23 -0300

    100-exercicios-java - condicionais - solucao exercicio VerificarDivisibilidade

--- 100-exercicios-java/src/main/java/br/com/condicionais/VerificaDivisibilidade.java
+++ 100-exercicios-java/src/main/java/br/com/condicionais/VerificaDivisibilidade.java
@@ -1,5 +1,7 @@
 package br.com.condicionais;
 
+import java.util.Scanner;
+
 /***
  * 
  * Faça um programa que leia tres numeros, e 
@@ -7,5 +9,26 @@ package br.com.condicionais;
  *
  */
 public class VerificaDivisibilidade {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("digite o primeiro numero : ");
+		int numero1 = scanner.nextInt();
+		
+		System.out.println("digite o segundo numero : ");
+		int numero2 = scanner.nextInt();
+		
+		System.out.println("digite o terceiro numero : ");
+		int numero3 = scanner.nextInt();
+		
+		int soma = numero1 + numero2 + numero3;
+		
+		if(soma % 5 == 0) {
+			System.out.println("a soma é divisivel por 5.");
+		} else {
+			System.out.println("a soma não é divisivel por 5");
+		}
+		
+		scanner.close();
+	}
 }
