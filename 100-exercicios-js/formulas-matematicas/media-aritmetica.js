549af3d2d0f4d7592412e6d242c91cba5302cc19
GeorgeSalu <george.salu10@gmail.com>
2023-12-16 11:01:45 -0300

    projeto 100-exercios de logica em js - media aritmetica

---
+++ 100-exercicios-js/formulas-matematicas/media-aritmetica.js
@@ -0,0 +1,6 @@
+let num1 = parseFloat(prompt("Digite o primeiro numero:"));
+let num2 = parseFloat(prompt("Digite o segundo numero:"));
+
+let media = (num1 + num2) / 2;
+
+console.log("a media aritmetica dos numeros", num1," e ", num2,"e:", media)
