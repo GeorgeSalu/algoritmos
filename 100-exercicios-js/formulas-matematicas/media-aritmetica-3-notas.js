ed597bc3dd9acbfd82f9b0aa05fc9018e609b697
GeorgeSalu <george.salu10@gmail.com>
2023-12-16 11:12:01 -0300

    projeto 100-exercios de logica em js - media aritmetica 3 notas

---
+++ 100-exercicios-js/formulas-matematicas/media-aritmetica-3-notas.js
@@ -0,0 +1,7 @@
+let nota1 = parseFloat(prompt("digite a primira nota:"));
+let nota2 = parseFloat(prompt("digite a segunda nota:"));
+let nota3 = parseFloat(prompt("digite a terceira nota:"));
+
+let media = (nota1+nota2+nota3) / 3;
+
+console.log("a media das notas é : ", media)
