65fc3963f98a3b4e809f3a9e9eed79318b4313e4
GeorgeSalu <george.salu10@gmail.com>
2023-12-18 09:35:01 -0300

    projeto 100-exercios de logica em js - media geometrica

---
+++ 100-exercicios-js/formulas-matematicas/media-geometrica.js
@@ -0,0 +1,8 @@
+
+let num1 = parseFloat(prompt("digite o primeiro numero:"));
+let num2 = parseFloat(prompt("digite o segundo numero:"));
+let num3 = parseFloat(prompt("digite o terceiro numero:"));
+
+let mediaGeometrica = Math.pow(num1 * num2 * num3, 1/3);
+
+console.log("a media geometrica dos numeros é :", mediaGeometrica)
