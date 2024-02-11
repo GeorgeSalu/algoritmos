9758069043449851fc0aca37f8db5cc2ac12c97a
GeorgeSalu <george.salu10@gmail.com>
2024-02-11 16:33:33 -0300

    projeto 100-exercios de logica em js - perimetro e are do retangulo

---
+++ 100-exercicios-js/formulas-matematicas/perimetro-e-area-retangulo.js
@@ -0,0 +1,9 @@
+let largura = parseFloat(prompt("digite a largura do rtangulo:"));
+let comprimento = parseFloat(prompt("digite o comprimento do retangulo:"));
+
+let perimetro = 2 * (largura + comprimento);
+
+let area = largura * comprimento;
+
+console.log("o perimetro do retangulo é :", perimetro);
+console.log("a area do retangulo é :", area)
