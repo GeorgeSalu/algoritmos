98c079ea5bbfc698d48dc51c5edddad368c0d63e
GeorgeSalu <george.salu10@gmail.com>
2024-02-13 19:18:04 -0300

    projeto 100-exercios de logica em js - volume da esfera

--- 100-exercicios-js/formulas-matematicas/volume-esfera.js
+++ 100-exercicios-js/formulas-matematicas/volume-esfera.js
@@ -1,3 +1,8 @@
+/*
+  crie um programa que solicite ao usuario o valor do raio
+  de uma esfera e calcule e exiba o seu volume
+*/
+
 let raio = parseFloat(prompt("digite o valor do raio da esfera:"));
 
 let volume = (4 / 3) * Math.PI * Math.pow(raio, 3);
