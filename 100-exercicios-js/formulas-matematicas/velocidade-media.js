72ce1aca41b312b2b4419516b7d44511dfe919a4
GeorgeSalu <george.salu10@gmail.com>
2024-02-13 18:33:19 -0300

    projeto 100-exercios de logica em js - velcidade media

---
+++ 100-exercicios-js/formulas-matematicas/velocidade-media.js
@@ -0,0 +1,6 @@
+let variacaoDeEspaco = parseFloat(prompt("digite a variacao de espaco:"));
+let variacaoDeTempo = parseFloat(prompt("digite a variacao de tempo:"));
+
+let velocidadeMedia = variacaoDeEspaco / variacaoDeTempo;
+
+console.log("a velocidade media do objeto :", velocidadeMedia)
