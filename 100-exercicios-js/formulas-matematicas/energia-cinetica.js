561b09e7ffd2e17dee0f6c62a1567953d0486e80
GeorgeSalu <george.salu10@gmail.com>
2024-02-13 18:38:41 -0300

    projeto 100-exercios de logica em js - energia cinetica

---
+++ 100-exercicios-js/formulas-matematicas/energia-cinetica.js
@@ -0,0 +1,6 @@
+let massa = parseFloat(prompt("digite a massa do objeto (m):"));
+let velocidade = parseFloat(prompt("digite a velocidade do objeto (v):"));
+
+let energiaCinetica = (massa * velocidade ** 2)/2;
+
+console.log("a energia cinetica do objeto é :", energiaCinetica)
