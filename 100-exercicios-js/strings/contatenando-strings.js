b888d6e4e9d1a6278026b819ae2300b11c33c5a8
GeorgeSalu <george.salu10@gmail.com>
2024-02-13 19:24:40 -0300

    projeto 100-exercios de logica em js - concatenando strings

---
+++ 100-exercicios-js/strings/contatenando-strings.js
@@ -0,0 +1,11 @@
+/*
+  crie um programa que leia duas palavras e as concatene,
+  exibindo a palavra resultante
+*/
+
+let palavra1 = prompt("digite a primeira palavra :");
+let palavra2 = prompt("digite a segunda palavra : ");
+
+let palavraResultante = palavra1 + palavra2;
+
+console.log("a palavra resultante :", palavraResultante)
