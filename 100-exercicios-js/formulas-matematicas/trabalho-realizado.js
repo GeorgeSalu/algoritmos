be0c928b4ceb43da380730c37bba63aca4fb97b2
GeorgeSalu <george.salu10@gmail.com>
2024-02-13 18:59:08 -0300

    projeto 100-exercios de logica em js - forca

---
+++ 100-exercicios-js/formulas-matematicas/trabalho-realizado.js
@@ -0,0 +1,6 @@
+let forca = parseFloat(prompt("digite a força aplicada (F):"));
+let distancia = parseFloat(prompt("digite a distancia percorrida (d):"));
+
+let trabalho = forca * distancia;
+
+console.log("o trabalho realizado pela forca é:", trabalho)
