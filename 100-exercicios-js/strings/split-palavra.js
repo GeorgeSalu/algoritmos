39c38d70f91e6f529323251cf7556c987cdb84e9
GeorgeSalu <george.salu10@gmail.com>
2024-02-13 19:32:12 -0300

    projeto 100-exercios de logica em js - exibir letras de uma palavra

---
+++ 100-exercicios-js/strings/split-palavra.js
@@ -0,0 +1,10 @@
+/*
+  17. faça um programa que receba uma palavra e 
+  exiba cada letra separadamente
+*/
+
+let palavra = prompt("digite uma palavra: ");
+
+for(let i = 0;i < palavra.length; i++) {
+  console.log(palavra[i])
+}
