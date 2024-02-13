a6f736bcde3053ba097c935431ab24bd402e90d8
GeorgeSalu <george.salu10@gmail.com>
2024-02-13 18:35:37 -0300

    projeto 100-exercios de logica em js - velcidade media

--- 100-exercicios-js/formulas-matematicas/velocidade-media.js
+++ 100-exercicios-js/formulas-matematicas/velocidade-media.js
@@ -1,3 +1,9 @@
+/*
+  11. escreva um programa que calcule a velocidade media de um objeto, utilizando
+  a formula v = deltaS/deltaT, onde v é a velocidade media, deltaS é a variação
+  de espaço e deltaT é a variação de tempo
+*/
+
 let variacaoDeEspaco = parseFloat(prompt("digite a variacao de espaco:"));
 let variacaoDeTempo = parseFloat(prompt("digite a variacao de tempo:"));
 
