f113655b85daeb4cda361aabe0b4c95d2c4b4d49
GeorgeSalu <george.salu10@gmail.com>
2024-02-13 19:02:16 -0300

    projeto 100-exercios de logica em js - forca

--- 100-exercicios-js/formulas-matematicas/trabalho-realizado.js
+++ 100-exercicios-js/formulas-matematicas/trabalho-realizado.js
@@ -1,3 +1,8 @@
+/*
+  13. escreva um programa que calcule o trabalho realizado por uma força
+  que atua sobre um objeto, utilizando a formula T = F * d, onde T é
+  o trablaho, F é a forca aplicada e d é a distancia percorrida pelo objeto
+*/
 let forca = parseFloat(prompt("digite a força aplicada (F):"));
 let distancia = parseFloat(prompt("digite a distancia percorrida (d):"));
 
