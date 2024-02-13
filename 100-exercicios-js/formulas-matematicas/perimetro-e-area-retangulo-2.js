3645389ee5abf32ee2ec1b6a2fbbd3dce9478cd5
GeorgeSalu <george.salu10@gmail.com>
2024-02-13 17:40:57 -0300

    projeto 100-exercios de logica em js - perimetro e are do retangulo 2

--- 100-exercicios-js/formulas-matematicas/perimetro-e-area-retangulo-2.js
+++ 100-exercicios-js/formulas-matematicas/perimetro-e-area-retangulo-2.js
@@ -1,3 +1,9 @@
+/*
+  escreva um programa que calcule o perimetro e a area de um triangulo,
+  utilizando as formulas p = a + b + c e A = (b * h)/ 2, onde a,b e c
+  são lados do traingulo e h é a altura relativa ao lado b
+*/
+
 let ladoA = parseFloat(prompt("digite o valor do lado a:"));
 let ladoB = parseFloat(prompt("digite o valor do lado b:"));
 let ladoC = parseFloat(prompt("digite o valor do lado c:"));
