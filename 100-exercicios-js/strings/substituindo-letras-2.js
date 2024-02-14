dad7610b27b33d2099b0ada3df485e8506a1f620
GeorgeSalu <george.salu10@gmail.com>
2024-02-13 23:06:47 -0300

    projeto 100-exercios de logica em js - substituindo letras

---
+++ 100-exercicios-js/strings/substituindo-letras-2.js
@@ -0,0 +1,13 @@
+let frase = prompt("digite uma frase");
+
+let caracteres = frase.split("");
+
+for(let i = 0;i < caracteres.length; i++) {
+  if(caracteres[i].toLowerCase() === "a") {
+    caracteres[i] = "e"
+  }
+}
+
+let novaFrase = caracteres.join("");
+
+console.log("nova frase: ", novaFrase)
