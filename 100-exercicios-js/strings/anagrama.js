45e81ecfff3dfdcc94b83b0f6f53d1ea8324d5e4
GeorgeSalu <george.salu10@gmail.com>
2024-02-14 19:28:37 -0300

    projeto 100-exercios de logica em js - anagrama

---
+++ 100-exercicios-js/strings/anagrama.js
@@ -0,0 +1,17 @@
+let palavra1 = prompt("digite a primeira palavra");
+let palavra2 = prompt("digite a segunda palavra");
+
+palavra1 = palavra1.toLowerCase();
+palavra2 = palavra2.toLowerCase();
+
+let arrayPalavra1 = palavra1.split("");
+let arrayPalavra2 = palavra2.split("");
+
+arrayPalavra1.sort();
+arrayPalavra2.sort();
+
+if(arrayPalavra1.join("") === arrayPalavra2.join("")) {
+  console.log("a segunda palavra é um anagrama da primeira");
+} else {
+  console.log("a segunda palavra não é um anagrama da primeira");
+}
