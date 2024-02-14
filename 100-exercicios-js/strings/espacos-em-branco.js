bebb9c987235073935f192c020d6351f6d4c241b
GeorgeSalu <george.salu10@gmail.com>
2024-02-14 20:10:34 -0300

    projeto 100-exercios de logica em js - quantidade de espacos em uma frase

---
+++ 100-exercicios-js/strings/espacos-em-branco.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * faça um programa que receba uma frase e exiba a
+ * quantidade de espaços em branco presentes na mesma
+ * 
+ */
+
+let frase = prompt("digite uma frase:");
+
+let quantidadeEspacos = 0;
+
+for(let i = 0; i < frase.length; i++) {
+  if(frase[i] === " ") {
+    quantidadeEspacos++;
+  }
+}
+
+console.log("a frase possui ", quantidadeEspacos," espacos em branco")
