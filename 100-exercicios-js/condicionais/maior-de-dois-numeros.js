f9f2564a7403a03de5a8fa7603985db4b894b42e
GeorgeSalu <george.salu10@gmail.com>
2024-02-15 20:03:53 -0300

    projeto 100-exercios de logica em js - maior numero entre dois

--- 100-exercicios-js/condicionais/maior-de-dois-numeros.js
+++ 100-exercicios-js/condicionais/maior-de-dois-numeros.js
@@ -12,7 +12,7 @@ numero1 = parseFloat(numero1);
 numero2 = parseFloat(numero2);
 
 if(numero1 > numero2) {
-  console.log("o primieiro numero é maior ", numero1);
+  console.log("o primeiro numero é maior ", numero1);
 } else if(numero2 > numero1) {
   console.log("o segundo numero é maior ", numero2);
 } else {
