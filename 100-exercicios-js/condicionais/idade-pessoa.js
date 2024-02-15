de79c79fe6deb14ab12c776cca2daa1c3b37943e
GeorgeSalu <george.salu10@gmail.com>
2024-02-14 22:06:36 -0300

    projeto 100-exercios de logica em js - verificar se maior de idade

--- 100-exercicios-js/condicionais/idade-pessoa.js
+++ 100-exercicios-js/condicionais/idade-pessoa.js
@@ -1,3 +1,10 @@
+/**
+ * 
+ * 26. faça um programa que solicite a idade de uma
+ * pessoa e exiba se ela é maior de idade ou não
+ * 
+ */
+
 let idade = prompt("digite a idade da pessoa");
 
 if(idade >= 18) {
