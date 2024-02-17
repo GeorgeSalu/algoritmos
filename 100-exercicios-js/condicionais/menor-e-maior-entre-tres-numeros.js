b18359d3f74439812707acf13532dbfa99f3cab2
GeorgeSalu <george.salu10@gmail.com>
2024-02-17 12:45:25 -0300

    projeto 100-exercios de logica em js - maior e menor numero entre tres

---
+++ 100-exercicios-js/condicionais/menor-e-maior-entre-tres-numeros.js
@@ -0,0 +1,24 @@
+let num1 = parseFloat(prompt("digite o primeiro numero: "));
+let num2 = parseFloat(prompt("digite o segundo numero: "));
+let num3 = parseFloat(prompt("digite o terceiro numero: "));
+
+let maior = num1;
+
+if(num2 > maior) {
+  maior = num2;
+}
+
+if(num3 > maior) {
+  maior = num3;
+}
+
+let menor = num1;
+if(num2 < menor) {
+  menor = num2;
+}
+if(num3 < menor) {
+  menor = num3;
+}
+
+console.log("o maior numero é : ", maior);
+console.log("o menor numero é : ", menor)
