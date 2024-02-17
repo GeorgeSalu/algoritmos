a4b5baed67da3c9c8c133cd20f2d0730a109ce96
GeorgeSalu <george.salu10@gmail.com>
2024-02-17 12:20:12 -0300

    projeto 100-exercios de logica em js - divisivel por 5

---
+++ 100-exercicios-js/condicionais/divisivel-por-5.js
@@ -0,0 +1,11 @@
+let num1 = parseInt(prompt("digite o primeiro numero: "));
+let num2 = parseInt(prompt("digite o segundo numero: "));
+let num3 = parseInt(prompt("digite o terceiro numero: "));
+
+let soma = num1 + num2 + num3;
+
+if(soma % 5 === 0) {
+  console.log("a soma dos numeros é divisivel por 5");
+} else {
+  console.log("a soma dos numeros não é divisivel por 5");
+}
