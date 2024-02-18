3a9fcb12c856ebaea076ea8ab7affa1caf135134
GeorgeSalu <george.salu10@gmail.com>
2024-02-17 21:53:16 -0300

    projeto 100-exercios de logica em js - aprovado, reprovado ou em recuperacao

--- 100-exercicios-js/condicionais/aprovado-reprovado-ou-recuperacao.js
+++ 100-exercicios-js/condicionais/aprovado-reprovado-ou-recuperacao.js
@@ -1,3 +1,12 @@
+/***
+ * 
+ * 40. faça um programa que leia tres notas de um aluno
+ * e informe se ele foi aprovado (nota final maior ou 
+ * igual a 7), reprovado (nota final menor que 4) ou 
+ * ficou de recuperacao (nota final entre 4 e 7)
+ * 
+ */
+
 let nota1 = parseFloat(prompt("digite a primeira nota : "));
 let nota2 = parseFloat(prompt("digite a segunda nota : "));
 let nota3 = parseFloat(prompt("digite a terceira nota : "));
