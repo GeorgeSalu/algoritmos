ecaf7129094082aca2c69f46ac8589136dfdf1a5
GeorgeSalu <george.salu10@gmail.com>
2024-02-17 22:13:27 -0300

    projeto 100-exercios de logica em js - dias da semana

---
+++ 100-exercicios-js/condicionais/dia-util-ou-fim-de-semana.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * 4. faça um programa que solicite o nome de um dia
+ * da semana e exiba se é um dia util (segunda a sexta-feira)
+ * ou um dia de fim de semana (sabado   e domingo)
+ * 
+ */
+
+let dia = prompt("digite o nome de um dia da semana : ").toLowerCase();
+
+if(dia === "sabado" || dia === "domingo") {
+  console.log("é um dia de fim de semana");
+} else if(dia === "segunda" || dia === "terca" || dia === "quarta" || dia === "quinta" || dia === "sexta"){
+  console.log("é um dia util");
+} else {
+  console.log("dia invalido")
+}
