4728a7e74aafa9de8d827511d8618ada44ce8719
GeorgeSalu <george.salu10@gmail.com>
2024-02-20 21:27:25 -0300

    projeto 100-exercios de logica em js - pares e impares

---
+++ 100-exercicios-js/lacos-de-repeticao/pares-e-impares.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * 49. escreva um programa que exiba os numeros 
+ * pares de 1 a 50 e os numeros impares de 51 a 
+ * 100 utlizando um laço de repeticao
+ * 
+ */
+
+console.log("numeros pares de 1 a 50:");
+let i = 2;
+while(i <= 50) {
+  console.log(i);
+  i += 2;
+}
+
+console.log("numeros impares de 51  a 100");
+let j = 51;
+while(j <= 100) {
+  console.log(j);
+  j += 2;
+}
