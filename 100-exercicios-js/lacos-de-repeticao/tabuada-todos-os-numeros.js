09ff84a24873d4511ada8437700ad764117056d7
GeorgeSalu <george.salu10@gmail.com>
2024-02-21 19:29:50 -0300

    projeto 100-exercios de logica em js - tabuada de todos os numeros

--- 100-exercicios-js/lacos-de-repeticao/tabuada-todos-os-numeros.js
+++ 100-exercicios-js/lacos-de-repeticao/tabuada-todos-os-numeros.js
@@ -1,3 +1,10 @@
+/***
+ * 
+ * 51. escreva um programa que imprima na tela 
+ * a tabuada de todos os numeros de 1 a a10
+ * 
+ */
+
 for(let i = 1;i <= 10; i++) {
   console.log(`tabuada do ${i}`);
   for(let j = 1;j <= 10;j++) {
