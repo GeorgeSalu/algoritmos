993eb5e85084a8bc3f0dbdf9186bca50db5a40e8
GeorgeSalu <george.salu10@gmail.com>
2024-02-21 21:20:11 -0300

    projeto 100-exercios de logica em js - numeros primos menores que N versao 2

---
+++ 100-exercicios-js/lacos-de-repeticao/todos-primos-menores-que-n-2.js
@@ -0,0 +1,20 @@
+let numero = parseInt(prompt("digite um numero : "));
+
+function isPrimo(num) {
+  if( num <= 1) {
+    return false;
+  }
+  for(let i = 2;i <= Math.sqrt(num); i++) {
+    if(num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log("numeros primos menores que "+numero+" :");
+for(let i = 2; i < numero;i++) {
+  if(isPrimo(i)) {
+    console.log(i)
+  }
+}
