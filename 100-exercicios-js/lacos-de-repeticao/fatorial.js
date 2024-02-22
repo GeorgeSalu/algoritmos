5d66a8ccc61476ff7cc4155852fceede1ab8984e
GeorgeSalu <george.salu10@gmail.com>
2024-02-21 22:31:20 -0300

    projeto 100-exercios de logica em js - fatorial

---
+++ 100-exercicios-js/lacos-de-repeticao/fatorial.js
@@ -0,0 +1,18 @@
+/***
+ * 
+ * 
+ * 58. crie um programa que exiba o fatorial de N, onde
+ * N é informado pelo usuario
+ * 
+ * 
+ */
+
+let N = parseInt(prompt("digite um numero : "));
+
+let fatorial = 1;
+
+for(let i = 2; i <= N; i++) {
+  fatorial *= i;
+}
+
+console.log("o fatorial de N é "+fatorial)
