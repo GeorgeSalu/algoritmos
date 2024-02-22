db73b6ce56390fd0038dbc93b3b689374a64604f
GeorgeSalu <george.salu10@gmail.com>
2024-02-21 22:50:15 -0300

    projeto 100-exercios de logica em js - soma numeros positivos

---
+++ 100-exercicios-js/lacos-de-repeticao/soma-numeros-positivos.js
@@ -0,0 +1,13 @@
+let numero;
+let soma = 0;
+
+do {
+  numero = parseInt(prompt("digite um numero negativo para sair : "));
+
+  if(numero >= 0) {
+    soma += numero;
+  }
+
+} while(numero >= 0)
+
+console.log("a soma dos numeroes positivos é : "+soma)
