1250a5c362814beb4961a93e8721fb6acb82ee79
GeorgeSalu <george.salu10@gmail.com>
2024-02-22 18:16:18 -0300

    projeto 100-exercios de logica em js - sequencia de fibonacci ate numero informado

--- 100-exercicios-js/lacos-de-repeticao/fibonati-ate-numero-informado.js
+++ 100-exercicios-js/lacos-de-repeticao/fibonati-ate-numero-informado.js
@@ -8,3 +8,18 @@
  * 
  * 
  */
+
+let numero = parseInt(prompt("digite um numero : "));
+
+let numeroAnterior = 0;
+let numeroAtual = 1;
+
+console.log("sequencia de fibonacci ate "+numero+" :");
+
+while(numeroAtual <= numero) {
+  console.log(numeroAtual);
+
+  let proximoNumero = numeroAnterior + numeroAtual;
+  numeroAnterior = numeroAtual;
+  numeroAtual = proximoNumero;
+}
