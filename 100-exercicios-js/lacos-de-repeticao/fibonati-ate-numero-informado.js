c8e8aed04a6fcc0bf0d64b3fbb5af93147f8f5b6
GeorgeSalu <george.salu10@gmail.com>
2024-02-21 23:30:08 -0300

    projeto 100-exercios de logica em js - fibonacci ate o numero informado (enunciado)

--- 100-exercicios-js/lacos-de-repeticao/fibonati-ate-numero-informado.js
+++ 100-exercicios-js/lacos-de-repeticao/fibonati-ate-numero-informado.js
@@ -1,7 +1,7 @@
 /**
  * 
  * 
- * 6. faça um programa que solicite ao usuario um numero 
+ * 61. faça um programa que solicite ao usuario um numero 
  * e exiba a sequancia de fibonacci ate o numero informado 
  * utlizando um laço de repetição
  * 
