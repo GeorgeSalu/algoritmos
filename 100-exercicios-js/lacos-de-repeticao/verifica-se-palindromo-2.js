6af7c5875c909fdae699ac6bb81ba368175caf4a
GeorgeSalu <george.salu10@gmail.com>
2024-02-23 20:14:51 -0300

    projeto 100-exercios de logica em js - verifica se é um palindromo versao 2

---
+++ 100-exercicios-js/lacos-de-repeticao/verifica-se-palindromo-2.js
@@ -0,0 +1,30 @@
+/***
+ * 
+ * 
+ * 66. escreva um programa que leia uma sequencia 
+ * de caracteres e indique se a mesma é um palindromo 
+ * ou não
+ * 
+ * 
+ * 
+ */
+
+function verificarPalindromo(texto) {
+  texto = texto.replace(/\s/g, '').toLowerCase();
+
+  for(let i = 0; i < texto.length / 2; i++) {
+    if(texto[i] !== texto[texto.length -1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const sequencia = prompt("digite uma sequencia de caracteres : ");
+
+if(verificarPalindromo(sequencia)) {
+  console.log("a sequencia é um palindromo")
+} else {
+  console.log("a sequencia não é um palindromo")
+}
