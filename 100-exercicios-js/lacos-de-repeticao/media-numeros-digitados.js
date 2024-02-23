f3269f8162f16e8e0ef9d3b7d503bca2fc0d1997
GeorgeSalu <george.salu10@gmail.com>
2024-02-23 18:15:21 -0300

    projeto 100-exercios de logica em js - media dos numeros digitados

---
+++ 100-exercicios-js/lacos-de-repeticao/media-numeros-digitados.js
@@ -0,0 +1,20 @@
+let sum = 0;
+let count = 0;
+let number;
+
+do {
+  number = parseFloat(prompt("digite um numero (ou zero para sair) : "));
+
+  if(number !== 0) {
+    sum += number;
+    count++;
+  }
+
+} while(number !== 0)
+
+if(count !== 0) {
+  let average = sum / count;
+  console.log("a media dos numeros digitados é : ", average);
+} else {
+  console.log("nenhum numero foi digitado : ");
+}
