5ab0b473d4269e9477f64fd03be7b9ac04d76c97
GeorgeSalu <george.salu10@gmail.com>
2024-02-23 19:05:48 -0300

    projeto 100-exercios de logica em js - menor e maior numeros de uma lista versao 2

---
+++ 100-exercicios-js/lacos-de-repeticao/maior-e-menor-numeros-de-uma-lista-2.js
@@ -0,0 +1,44 @@
+/***
+ * 
+ * 
+ * 63. escreva um programa que solicite ao usuario uma lista de numeros, 
+ * ate o usuario digitar o numero zero, e exiba o maior e o menor numero
+ * desta lista
+ * 
+ * 
+ */
+
+let numbers = [];
+let number;
+
+do {
+  number = parseFloat(prompt("digite um numero : "));
+
+  if(number !== 0 && !isNaN(number)) {
+    numbers.push(number);
+  }
+
+}while(number !== 0)
+
+if(numbers.length > 0) {
+  let min = numbers[0];
+  let max = numbers[0];
+
+  for(let i = 1; i < numbers.length; i++) {
+    let num = numbers[i];
+
+    if(num < min) {
+      min = num;
+    }
+
+    if(num > max) {
+      max = num;
+    }
+  }
+
+  console.log("o menor numero : "+min);
+  console.log("o maior numero é :"+max);
+
+} else {
+  console.log("nenhum numero foi digitado")
+}
