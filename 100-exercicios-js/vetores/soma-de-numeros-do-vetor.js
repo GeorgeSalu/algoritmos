da4043db68f13301438d4550a1177debbfcd377a
GeorgeSalu <george.salu10@gmail.com>
2024-02-25 09:41:31 -0300

    projeto 100-exercios de logica em js - soma dos elementos do vetor

---
+++ 100-exercicios-js/vetores/soma-de-numeros-do-vetor.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * 
+ * 
+ * 71crie um programa que leia um vetor de numeros inteiros
+ * e exiba a soma de todos os elementos
+ * 
+ * 
+ * 
+ */
+
+const quantidade = prompt("digite a quantidade de elementos do vetor : ");
+
+const vetor = [];
+
+for(let i = 0;i < quantidade;i++) {
+  const numero = parseInt(prompt(`digite o elemento ${i + 1} : `));
+  vetor.push(numero);
+}
+
+let soma = 0;
+for(let i = 0;i < vetor.length; i++) {
+  soma += vetor[i];
+}
+
+console.log("a soma dos elementos é : "+soma)
