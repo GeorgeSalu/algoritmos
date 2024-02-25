7dcfc96c5ac2f16c9c7a0e75c7febd9041b03123
GeorgeSalu <george.salu10@gmail.com>
2024-02-25 15:04:22 -0300

    projeto 100-exercios de logica em js - soma dos elementos do vetor

--- 100-exercicios-js/vetores/soma-dos-elementos-dos-vetores.js
+++ 100-exercicios-js/vetores/soma-dos-elementos-dos-vetores.js
@@ -11,11 +11,15 @@
  * 
  * 
  */
+
+// solicite ao usuario a quantidade de elementos dos vetores
 const quantidade = parseInt(prompt("digite a quantidade de elementos dos vetores : "));
 
+//c ria dois vetores vazios
 const vetor1 = [];
 const vetor2 = [];
 
+// preenche os vetores com os numeros informados pelo usuario
 for(let i = 0;i < quantidade; i++) {
   const numero1 = parseInt(prompt(`digite o elemento ${i + 1} do vetor 1 :`));
   vetor1.push(numero1);
@@ -24,11 +28,15 @@ for(let i = 0;i < quantidade; i++) {
   vetor2.push(numero2);
 }
 
+// cria um vetor para armazenar a soma dos elementos correspondentes dos dois vetores
 const vetorSoma = [];
 
+
+// calcula a soma dos elementos correspondentes dos dois vetores
 for(let i = 0; i < quantidade; i++) {
   const soma = vetor1[i] + vetor2[i];
   vetorSoma.push(soma)
 }
 
+// exibe o vetor com a soma dos elementos correspondentes
 console.log(`vetor com a soma dos elementos corespondentes : ${vetorSoma}`)
