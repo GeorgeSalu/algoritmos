8c3186a5fb47f47772d459dfb00759d26c35d991
GeorgeSalu <george.salu10@gmail.com>
2024-02-25 16:11:57 -0300

    projeto 100-exercios de logica em js - verifica se vetor em ordem crescente (falta terminar)

---
+++ 100-exercicios-js/vetores/vetor-ordem-crescente.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * 
+ * 
+ * 75. faça um programa que leia um vetor de numeros inteiros
+ * e verifique se ele esta em ordem crescente.
+ * 
+ * 
+ * 
+ */
+const quantidade = parseInt(prompt("digite a quantidade de elementos do vetor : "));
+
+const vetor = [];
+
+for(let i = 0; i < quantidade; i++) {
+  const numero = parseInt(prompt(`digite o elemento ${i + 1} do vetor : `));
+  vetor.push(numero);
+}
+
+console.log(vetor);
