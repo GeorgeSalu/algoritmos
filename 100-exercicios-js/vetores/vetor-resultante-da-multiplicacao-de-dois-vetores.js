dceb6ff15c423f045a9232de8669afe839767dd2
GeorgeSalu <george.salu10@gmail.com>
2024-02-26 19:32:01 -0300

    projeto 100-exercios de logica em js - vetor resultante da multiplicacao de dois vetores (enunciado)

---
+++ 100-exercicios-js/vetores/vetor-resultante-da-multiplicacao-de-dois-vetores.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 79. escreva um programa que leia dois vetores de numeros
+ * inteiros com o mesmo tamanho e exiba um novo vetor com os 
+ * elementos resultantes da multiplicação dos elementos corres-
+ * pondentes dos dois vetores.
+ * 
+ * 
+ * 
+ * 
+ */
