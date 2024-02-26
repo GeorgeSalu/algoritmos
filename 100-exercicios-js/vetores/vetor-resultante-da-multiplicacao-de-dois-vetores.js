154a40a9e11183c1cef1da96b5cecca6fcf89127
GeorgeSalu <george.salu10@gmail.com>
2024-02-26 20:11:01 -0300

    projeto 100-exercios de logica em js - vetor resultante da multiplicacao de dois vetores

--- 100-exercicios-js/vetores/vetor-resultante-da-multiplicacao-de-dois-vetores.js
+++ 100-exercicios-js/vetores/vetor-resultante-da-multiplicacao-de-dois-vetores.js
@@ -12,3 +12,24 @@
  * 
  * 
  */
+const quantidade = parseInt(prompt("digite a quantidade de elementos do vetor : "));
+
+const vetor1 = [];
+const vetor2 = [];
+
+for(let i = 0;i < quantidade; i++) {
+  const numero1 = parseInt(prompt(`digite o elemento ${i + 1} do primeiro vetor : `));
+  vetor1.push(numero1);
+
+  const numero2 = parseInt(prompt(`digite o elemento ${i + 1} do segundo vetor : `));
+  vetor2.push(numero2);
+}
+
+const vetorResultado = [];
+
+for(let i = 0; i < quantidade ; i++) {
+  const resultado = vetor1[i] * vetor2[i];
+  vetorResultado.push(resultado);
+}
+
+console.log(`vetor resultado : ${vetorResultado}`)
