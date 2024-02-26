626da3fc04b63357fc2a66fd5be59de8b40b82f1
GeorgeSalu <george.salu10@gmail.com>
2024-02-26 18:48:20 -0300

    projeto 100-exercios de logica em js - segundo maior elemento do vetor versao 2

---
+++ 100-exercicios-js/vetores/segundo-maior-elemento-do-vetor-2.js
@@ -0,0 +1,17 @@
+const quantidade = parseInt(prompt("digite a quantiade de elementos do vetor : "));
+
+const vetor = [];
+
+for(let i = 0;i < quantidade; i++) {
+  const numero = parseInt(prompt(`digite o elemento ${i + 1} do vetor : `));
+  vetor.push(numero);
+}
+
+vetor.sort((a, b) => a - b);
+
+if(vetor.length >= 2) {
+  const segundoMaior = vetor[vetor.length - 2];
+  console.log("segundo maior elemento : ", segundoMaior);
+} else {
+  console.log("o vetor precisa ter pelo menos dois elementos")
+}
