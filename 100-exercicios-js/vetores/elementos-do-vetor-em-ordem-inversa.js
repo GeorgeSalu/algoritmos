6b95110f9b9f7b8df9322d1ccf2391ae60d2b681
GeorgeSalu <george.salu10@gmail.com>
2024-02-26 17:58:49 -0300

    projeto 100-exercios de logica em js - elementos inversos do vetor

---
+++ 100-exercicios-js/vetores/elementos-do-vetor-em-ordem-inversa.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * 
+ * 
+ * 76, escreva um programa que leia um vetor de numeros 
+ * inteiros e exiba os elementos na ordem inversa. 
+ * 
+ * 
+ * 
+ * 
+ */
+const quantiadde = parseInt(prompt("digite a quantidade de elementos do vetor : "));
+
+const vetor = [];
+
+for(let i = 0; i < quantiadde; i++) {
+  const numero = parseInt(prompt(`digite o elemento ${i + i} do vetor : `));
+  vetor.push(numero);
+}
+
+const vetorInverso = [];
+
+for(let i = quantiadde - 1; i >= 0; i--) {
+  vetorInverso.push(vetor[i]);
+}
+
+console.log("vetor original : ", vetor);
+console.log("vetor inverso  : ", vetorInverso)
