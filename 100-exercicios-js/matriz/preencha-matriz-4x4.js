24e0a5d2612978dd9aff60ac3ce8d21aff4d0d4f
GeorgeSalu <george.salu10@gmail.com>
2024-02-27 17:29:35 -0300

    projeto 100-exercios de logica em js - preenche uma matriz 4x4 com valores aleatorios

---
+++ 100-exercicios-js/matriz/preencha-matriz-4x4.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * 
+ * 
+ * faça um programa que preencha uma matriz 4x4 
+ * com valores aleatories e imprima
+ * 
+ * 
+ * 
+ * 
+ */
+
+const matriz = [];
+for(let i = 0;i < 4;i++) {
+  matriz[i] = []
+}
+
+for(let i = 0; i < 4; i++) {
+  for(let j = 0;j < 4; j++) {
+    matriz[i][j] = Math.floor(Math.random() * 100);
+  }
+}
+
+console.log("matriz original");
+console.log(matriz)
