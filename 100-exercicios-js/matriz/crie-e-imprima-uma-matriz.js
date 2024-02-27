49547afa0dc0d25107ad5d63fabe2dd9374049d5
GeorgeSalu <george.salu10@gmail.com>
2024-02-26 22:26:36 -0300

    projeto 100-exercios de logica em js - crie e imprima uma matriz 3x3

---
+++ 100-exercicios-js/matriz/crie-e-imprima-uma-matriz.js
@@ -0,0 +1,16 @@
+
+// cria uma matriz vazia com 3 linhas e 3 colunas
+const matriz = [];
+for(let i = 0;i < 3 ; i++) {
+  matriz[i] = [];
+}
+
+// preenche a matriz com valores informados pelo usuario
+for(let i = 0;i < 3;i++) {
+  for(let j = 0;j < 3;j ++) {
+    const valor = parseInt(prompt(`digite o valor da posicao [${i}|${j}] : `));
+    matriz[i][j] = valor;
+  }
+}
+
+console.log(matriz)
