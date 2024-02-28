72e586fc76e0694fd3d9f4e2f96299c573b0c01a
GeorgeSalu <george.salu10@gmail.com>
2024-02-28 19:05:46 -0300

    projeto 100-exercios de logica em js - soma dos indices pares da matriz

---
+++ 100-exercicios-js/matriz/soma-dos-indices-pares.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 85. faça um programa que leia uam matriz 3x3 e 
+ * calcule a media dos valores presentes nas posições
+ * pares (soma dos indices par) da matriz
+ * 
+ * 
+ * 
+ * 
+ */
+const matriz = [];
+for(let i = 0;i < 3; i++) {
+  matriz[i] = [];
+}
+
+// preenche a matriz
+for(let i = 0;i < 3;i++) {
+  for(let j = 0; j < 3; j++) {
+    matriz[i][j] = parseInt(prompt(`digite o valor da posicao [${i}]|[${j}] : `));
+  }
+}
+
+let soma = 0;
+let contador = 0;
+
+for(let i = 0; i < 3; i++) {
+  for(let j = 0; j < 3; j++) {
+    if((i + j) % 2 === 0) {
+      soma += matriz[i][j];
+      contador++;
+    }
+  }
+}
+
+const media = soma / contador;
+
+console.log(`resultado ${media.toFixed(2)}`)
