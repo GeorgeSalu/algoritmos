8967e9ef920c5330c0e318bde963a6dd56069a98
GeorgeSalu <george.salu10@gmail.com>
2024-02-28 18:08:26 -0300

    projeto 100-exercios de logica em js - preencha matriz 5x5

---
+++ 100-exercicios-js/matriz/preencha-matriz-5x5.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 84. faça um programa que preencha uma matriz 
+ * 5x5 com valores inteiros
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+const matriz = [];
+for(let i = 0; i < 5; i++) {
+  matriz[i] = [];
+}
+
+// preenche matriz
+for(let i = 0; i < 5; i++) {
+  for(let j = 0; j < 5; j++) {
+    matriz[i][j] = parseInt(prompt(`digite o valor da posicao [${i}]|[${j}] : `));
+  }
+}
+
+console.log(matriz)
