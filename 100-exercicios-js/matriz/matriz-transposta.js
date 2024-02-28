dafa9578be379d3bdaef5728ceddd857d5767c43
GeorgeSalu <george.salu10@gmail.com>
2024-02-28 17:19:13 -0300

    projeto 100-exercios de logica em js - exiba matriz transposta

---
+++ 100-exercicios-js/matriz/matriz-transposta.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * 
+ * 
+ * 82. faça um programa que preencha uam matriz 4x4
+ * com valores aleatorios e exiba a matriz transposta
+ * 
+ * 
+ * 
+ * 
+ */
+const matriz = [];
+for(let i = 0;i < 4;i++) {
+  matriz[i] = [];
+}
+
+// preenche com valores aleatorios 
+for(let i = 0; i < 4;i++) {
+  for(let j = 0; j < 4; j++) {
+    matriz[i][j] = Math.floor(Math.random() * 100);
+  }
+}
+
+// exibe a matriz transposta
+console.log("matriz original : ");
+for(let i = 0; i < 4;i++) {
+  console.log(matriz[i].join(" "));
+}
+
+// obtem a matriz transposta
+const transposta = [];
+for(let i = 0;i < 4; i++) {
+  transposta[i] = [];
+  for(let j = 0; j < 4; j++) {
+    transposta[i][j] = matriz[j][i];
+  }
+}
+
+//exiba a matriz transposta
+console.log("matriz transposta")
+for(let i = 0;i < 4;i++) {
+  console.log(transposta[i].join(" "))
+}
