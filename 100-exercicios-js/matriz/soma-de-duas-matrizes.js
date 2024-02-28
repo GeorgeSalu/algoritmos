af11c03cc415f05fc4578bbe39f886d082435491
GeorgeSalu <george.salu10@gmail.com>
2024-02-28 17:39:47 -0300

    projeto 100-exercios de logica em js - soma das matrizes

---
+++ 100-exercicios-js/matriz/soma-de-duas-matrizes.js
@@ -0,0 +1,53 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 83. faça um programa que leia duas matrizes
+ * 2x2 e exiba a soma das duas matriz
+ * 
+ * 
+ * 
+ * 
+ */
+console.log("digite os elementos da primeira matriz : ");
+const matriz1 = lerMatriz();
+
+console.log("digite os elementos da segunda matriz : ");
+const matriz2 = lerMatriz();
+
+console.log("matriz 1");
+exibirMatriz(matriz1);
+
+console.log("matriz 2");
+exibirMatriz(matriz2)
+
+
+const matrizSoma = [];
+for(let i = 0; i < 2;i++) {
+  matrizSoma[i] = [];
+  for(let j = 0;j < 2; j++) {
+    matrizSoma[i][j] = matriz1[i][j] + matriz2[i][j];
+  }
+}
+
+console.log("soma das matrizes");
+exibirMatriz(matrizSoma)
+
+
+function lerMatriz() {
+  const matriz = [];
+  for(let i = 0;i < 2; i++) {
+    matriz[i] = [];
+    for(let j = 0;j < 2;j++) {
+      matriz[i][j] = parseInt(prompt(`digite o valor da possicao [${i}]|[${j}] : `));
+    }
+  }
+  return matriz;
+}
+
+function exibirMatriz(matriz) {
+  for(let i = 0;i < 2;i++) {
+    console.log(matriz[i].join(" "))
+  }
+}
