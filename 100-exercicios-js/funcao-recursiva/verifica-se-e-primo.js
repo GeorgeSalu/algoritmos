b4f096eae40a0317232db08b74ca1227e16c09ec
GeorgeSalu <george.salu10@gmail.com>
2024-03-01 18:45:32 -0300

    projeto 100-exercios de logica em js - verifica se é primo recursividade

---
+++ 100-exercicios-js/funcao-recursiva/verifica-se-e-primo.js
@@ -0,0 +1,19 @@
+function isPrimo(n, divisor = 2) {
+  if(n <= 1) {
+    return false;
+  }
+
+  if(divisor === n) {
+    return true;
+  }
+
+  if(n % divisor === 0) {
+    return false;
+  }
+
+  return isPrimo(n, divisor + 1);
+}
+
+const numero = 17;
+const resultado = isPrimo(numero);
+console.log(`o numero ${numero} é primo ? ${resultado} `)
