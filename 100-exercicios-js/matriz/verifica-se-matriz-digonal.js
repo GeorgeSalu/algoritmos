96dccc8c73a0a2ffc08aa885523d52d73204c638
GeorgeSalu <george.salu10@gmail.com>
2024-03-01 17:13:24 -0300

    projeto 100-exercios de logica em js - verifica se matriz diagonal

---
+++ 100-exercicios-js/matriz/verifica-se-matriz-digonal.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 89. faça um programa que leia uma matriz 4x4 e verifique 
+ * se ela é uma matriz diagonal. isto é. se todos os elementos
+ * fora da diagonal principal são iguais a zero
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
