c5d96fcae4ee4363339ec99118152db8df0081f4
GeorgeSalu <george.salu10@gmail.com>
2024-03-01 19:04:50 -0300

    projeto 100-exercios de logica em js - soma dos digitos de um inteiro recursividade

---
+++ 100-exercicios-js/funcao-recursiva/soma-dos-digitos-de-um-inteiro.js
@@ -0,0 +1,14 @@
+function somaDigitos(numero) {
+
+  if(numero < 10) {
+    return numero
+  }
+
+  const resto = numero % 10;
+  const quociente = Math.floor(numero / 10);
+  return resto + somaDigitos(quociente);
+}
+
+const numero = 12345;
+const resultado = somaDigitos(numero);
+console.log(`a soma dos digitos de ${numero} é ${resultado}`)
