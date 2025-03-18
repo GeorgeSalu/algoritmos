d18dfb285fed731e57d9dddd51b2183e8a1fdca9
George Salu <george.salu10@gmail.com>
2025-03-18 17:13:29 -0300

    200-exercicios-java - exercicio primeiros passos

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo2/PrimeirosPassos1.java
@@ -0,0 +1,11 @@
+package br.com.capitulo2;
+
+public class PrimeirosPassos1 {
+	public static void main(String[] args) {
+		// exibindo "Ola mundo" no console
+		System.out.println("Ola, Mundo");
+		
+		// modificando para exibir o nome
+		System.out.println("meu nome é george");
+	}
+}
