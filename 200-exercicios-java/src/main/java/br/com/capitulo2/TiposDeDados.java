0a8cb9ac987166828681556261d87de68a42a4ae
George Salu <george.salu10@gmail.com>
2025-03-18 17:26:02 -0300

    200-exercicios-java - exercicio tipos de dados

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo2/TiposDeDados.java
@@ -0,0 +1,15 @@
+package br.com.capitulo2;
+
+public class TiposDeDados {
+	public static void main(String[] args) {
+		int idade = 25;
+		double altura = 1.75;
+		char inicial = 'M';
+		boolean estudante = true;
+		
+		System.out.println("Idade : "+idade);
+		System.out.println("Altura : "+altura);
+		System.out.println("Inicial do nome : "+inicial);
+		System.out.println("é estudante : "+estudante);
+	}
+}
