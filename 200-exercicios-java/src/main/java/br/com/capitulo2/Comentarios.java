6da476257512be375c5fcf444cb4582c7f610842
George Salu <george.salu10@gmail.com>
2025-03-18 17:15:19 -0300

    200-exercicios-java - exercicio comentarios

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo2/Comentarios.java
@@ -0,0 +1,14 @@
+package br.com.capitulo2;
+
+public class Comentarios {
+	public static void main(String[] args) {
+		// este é um comentario de linha, usado para comentar uma unica linha
+		
+		/*
+		 Este é um comentario de bloco
+		 que pode usar varias linhas 
+		*/
+		
+		System.out.println("comentarios adicionados ao codigo");
+	}
+}
