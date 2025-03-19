1bd6ab56ccdccf2d4fa21488b50300a531de0a1e
George Salu <george.salu10@gmail.com>
2025-03-19 11:48:12 -0300

    200-exercicios-java - exercicio constantes

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo2/Constantes.java
@@ -0,0 +1,11 @@
+package br.com.capitulo2;
+
+public class Constantes {
+	public static void main(String[] args) {
+		
+		final double VELOCIDADE_DA_LUZ = 299792458; // em metros por segundos
+		
+		System.out.println("velocidade da luz : "+VELOCIDADE_DA_LUZ+" m/s");
+		
+	}
+}
