9a14fce4f086131c6637165882b0711133cf1cf5
George Salu <george.salu10@gmail.com>
2025-03-19 12:33:01 -0300

    200-exercicios-java - exercicio tipos de variaveis

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo2/TiposDeVariaveis.java
@@ -0,0 +1,17 @@
+package br.com.capitulo2;
+
+public class TiposDeVariaveis {
+
+	// variavel global
+	static int variavelGlobal = 10;
+	
+	public static void main(String[] args) {
+		
+		// variavel local
+		int variavelLocal = 5;
+		
+		System.out.println("Valor da variavel global : "+variavelGlobal);
+		System.out.println("Valor da variavel local : "+variavelLocal);
+		
+	}
+}
