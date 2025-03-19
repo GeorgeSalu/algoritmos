6a24fe9b2ef309e2b5a3b0417ce9849555e754b5
George Salu <george.salu10@gmail.com>
2025-03-19 12:34:30 -0300

    200-exercicios-java - exercicio tipos de variaveis

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo2/FormatacaoSaida.java
@@ -0,0 +1,9 @@
+package br.com.capitulo2;
+
+public class FormatacaoSaida {
+	public static void main(String[] args) {
+		double valor = 123.456789;
+		
+		System.out.printf("valor formatado : %.2f%n", valor);
+	}
+}
