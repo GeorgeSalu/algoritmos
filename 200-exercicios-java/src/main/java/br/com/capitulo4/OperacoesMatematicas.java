8f3436b7b48906e4c2e201c7b6d6fe19288f920b
George Salu <george.salu10@gmail.com>
2025-03-20 20:57:24 -0300

    200-exercicios-java - exercicio operacoes matematicas

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo4/OperacoesMatematicas.java
@@ -0,0 +1,42 @@
+package br.com.capitulo4;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * Desenvolva um programa que leia dois números e um operador (+, -, *, /) e realize a operação correspondente. Exiba o resultado no console.
+ * 
+ * */
+public class OperacoesMatematicas {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("digite o primeiro numero : ");
+		double numero1 = scanner.nextDouble();
+		
+		System.out.println("digite o segundo numero : ");
+		double numero2 = scanner.nextDouble();
+		
+		System.out.println("digite o operador (+,-,*,/) : ");
+		char operador = scanner.next().charAt(0);
+		
+		switch(operador) {
+			case '+':
+				System.out.println("o resultado : "+(numero1+numero2));
+				break;
+			case '-':
+				System.out.println("o resultado : "+(numero1-numero2));
+				break;
+			case '*':
+				System.out.println("o resultado : "+(numero1*numero2));
+				break;
+			case '/':
+				System.out.println("o resultado : "+(numero1/numero2));
+				break;
+			default:
+				System.out.println("operador invalido");
+		}
+		
+		scanner.close();
+	}
+}
