794e75ad4cf8c7370ddd5ad1c8c1955e339520ce
George Salu <george.salu10@gmail.com>
2025-03-20 21:55:13 -0300

    200-exercicios-java - exercicio contagem decrescente

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo4/ContagemDecrescente.java
@@ -0,0 +1,15 @@
+package br.com.capitulo4;
+
+/*
+ * 
+ * Crie um programa que exiba os números de 10 a 1, em ordem decrescente, usando um laço for.
+ * 
+ * 
+ * */
+public class ContagemDecrescente {
+	public static void main(String[] args) {
+		for(int i = 10;i >= 1;i --) {
+			System.out.println(i);
+		}
+	}
+}
