5b5a7ce65a3cefa2bebfbadeb174b8c03188c808
George Salu <george.salu10@gmail.com>
2025-03-21 12:17:43 -0300

    200-exercicios-java - lacos de repeticao - exercicio soma de numeros

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo4/SomaDeNumeros.java
@@ -0,0 +1,18 @@
+package br.com.capitulo4;
+
+/*
+ * 
+ * 
+ * Escreva um programa que calcule a soma de todos os números de 1 a 100.
+ * 
+ * 
+ * */
+public class SomaDeNumeros {
+	public static void main(String[] args) {
+		int soma = 0;
+		for(int i = 1;i <= 100; i++) {
+			soma += i;
+		}
+		System.out.println("soma de 1 a 100 : "+soma);
+	}
+}
