17ddbe7704816b94b01e89dad32864e52acdfce2
George Salu <george.salu10@gmail.com>
2025-03-20 21:33:25 -0300

    200-exercicios-java - exercicio contagem crescente

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo4/ContagemCrescente.java
@@ -0,0 +1,14 @@
+package br.com.capitulo4;
+
+/*
+ * 
+ * Escreva um programa que exiba os números de 1 a 10, em ordem crescente, usando um laço for.
+ * 
+ * */
+public class ContagemCrescente {
+	public static void main(String[] args) {
+		for(int i = 1;i <= 10;i++) {
+			System.out.println(i);
+		}
+	}
+}
