c015961ad8602d4ee1b18dd7f540aa823d02d414
George Salu <george.salu10@gmail.com>
2025-03-20 21:58:17 -0300

    200-exercicios-java - lacos de repeticao exercicio numeros pares

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo4/NumeroPares.java
@@ -0,0 +1,15 @@
+package br.com.capitulo4;
+
+/*
+ * 
+ * Desenvolva um programa que exiba todos os números pares de 1 a 20.
+ * 
+ * 
+ * */
+public class NumeroPares {
+	public static void main(String[] args) {
+		for(int i = 2;i <= 20; i+=2) {
+			System.out.println(i);
+		}
+	}
+}
