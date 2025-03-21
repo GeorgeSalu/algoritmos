226db841e2ec4c1c038e6f5763f1ba0fde767850
George Salu <george.salu10@gmail.com>
2025-03-21 13:07:56 -0300

    200-exercicios-java - lacos de repeticao - exercicio numeros impares

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo4/NmerosImpares.java
@@ -0,0 +1,15 @@
+package br.com.capitulo4;
+
+/*
+ * 
+ * Escreva um programa que exiba todos os números ímpares entre 1 e 50.
+ * 
+ * 
+ * */
+public class NmerosImpares {
+	public static void main(String[] args) {
+		for(int i = 1;i <= 50;i += 2) {
+			System.out.println(i);
+		}
+	}
+}
