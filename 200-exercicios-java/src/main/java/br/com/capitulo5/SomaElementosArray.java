a6dcc96c18c09e370fdd03489d387a920e110053
George Salu <george.salu10@gmail.com>
2025-03-23 22:36:16 -0300

    200-exercicios-java - arrays e colecoes - exercicio soma dos elementos de um array

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo5/SomaElementosArray.java
@@ -0,0 +1,26 @@
+package br.com.capitulo5;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * Desenvolva um programa que leia 5 números inteiros do usuário, armazene-os em um array e calcule a soma de todos os elementos.
+ * 
+ * 
+ */
+public class SomaElementosArray {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int numeros[] = new int[5];
+		int soma = 0;
+		
+		for(int i = 0;i < numeros.length;i++) {
+			System.err.println("digite o numero "+(i+1)+" :");
+			numeros[i] = scanner.nextInt();
+			soma += numeros[i];
+		}
+		
+		System.err.println("A soma dos elementos do array é : "+soma);
+	}
+}
