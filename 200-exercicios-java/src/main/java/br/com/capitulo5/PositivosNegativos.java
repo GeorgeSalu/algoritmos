364d6d9ea9cff6d3c9e014ef7a2c8a54b672e597
George Salu <george.salu10@gmail.com>
2025-03-23 23:49:22 -0300

    200-exercicios-java - arrays e colecoes - exercicio contagem de valores positivos e negativos

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo5/PositivosNegativos.java
@@ -0,0 +1,35 @@
+package br.com.capitulo5;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * Escreva um programa que leia 15 números inteiros e, em seguida, exiba quantos desses números são positivos e quantos são negativos.
+ * 
+ * 
+ * */
+public class PositivosNegativos {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int numeros[] = new int[15];
+		int positicos = 0, negativos = 0;
+		
+		for(int i = 0;i < numeros.length; i++) {
+			System.out.println("digite o numero : "+(i+1)+" : ");
+			numeros[i] = scanner.nextInt();
+		}
+		
+		for(int numero: numeros) {
+			if(numero > 0) {
+				positicos++;
+			} else if(numero < 0) {
+				negativos++;
+			}
+		}
+		
+		System.out.println("quantidade de numeros positivos : "+positicos);
+		System.out.println("quntiadde de numeros negativos : "+negativos);
+		
+	}
+}
