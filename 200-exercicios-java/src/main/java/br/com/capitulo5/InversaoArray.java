7b47f11e4ae6d472684ace1a7fafe3e917ade113
George Salu <george.salu10@gmail.com>
2025-03-24 12:58:32 -0300

    200-exercicios-java - arrays e colecoes - exercicio inversao array

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo5/InversaoArray.java
@@ -0,0 +1,28 @@
+package br.com.capitulo5;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * Enunciado: Crie um programa que leia 6 números inteiros e armazene-os em um array. Depois, exiba os valores do array na ordem inversa.
+ * 
+ * 
+ * */
+public class InversaoArray {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int numeros[] = new int[6];
+		
+		for(int i = 0;i < numeros.length; i++) {
+			System.out.println("digite o numero "+(i+1)+" :");
+			numeros[i] = scanner.nextInt();
+		}
+		
+		System.out.println("numeros em ordem inversa:");
+		for(int i = numeros.length-1; i >= 0; i--) {
+			System.err.println(numeros[i]);
+		}
+		
+	}
+}
