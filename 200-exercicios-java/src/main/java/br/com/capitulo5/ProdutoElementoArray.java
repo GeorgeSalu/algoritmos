a0788476fc77687de02ad14756b54906e0dd3f69
George Salu <george.salu10@gmail.com>
2025-03-24 17:39:01 -0300

    200-exercicios-java - arrays e colecoes - exercicio produto dos elementos de um array

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo5/ProdutoElementoArray.java
@@ -0,0 +1,28 @@
+package br.com.capitulo5;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * Crie um programa que leia 6 números inteiros e calcule o produto de todos os valores do array
+ * 
+ * 
+ * */
+public class ProdutoElementoArray {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int numeros[] = new int[6];
+		int produto = 1;
+		
+		for(int i = 0;i < numeros.length; i++) {
+			System.out.println("digite o numero "+(i+1)+" :");
+			numeros[i] = scanner.nextInt();
+			produto *= numeros[i];
+		}
+		
+		System.out.println("o produto dos elementos do array é : "+produto);
+		
+		scanner.close();
+	}
+}
