781afccc46451d758aa471f6b8decee445429532
George Salu <george.salu10@gmail.com>
2025-03-25 11:26:13 -0300

    200-exercicios-java - arrays e colecoes - exercicio matriz identidade

--- 200-exercicios-java/src/main/java/br/com/capitulo5/MatrizIdentidade.java
+++ 200-exercicios-java/src/main/java/br/com/capitulo5/MatrizIdentidade.java
@@ -23,7 +23,7 @@ public class MatrizIdentidade {
 		System.out.println("matriz identidade 4x4");
 		for(int i = 0;i < 4; i++) {
 			for(int j = 0;j < 4; j++) {
-				System.out.println(matriz[i][j] + " ");
+				System.out.print(matriz[i][j] + " ");
 			}
 			System.out.println();
 		}
