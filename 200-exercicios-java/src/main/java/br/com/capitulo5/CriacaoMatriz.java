bd81cfa2825792e8f99fb58a7430782cd45b8229
George Salu <george.salu10@gmail.com>
2025-03-25 11:29:22 -0300

    200-exercicios-java - arrays e colecoes - exercicio criacao matriz (melhora a saida no console da matriz)

--- 200-exercicios-java/src/main/java/br/com/capitulo5/CriacaoMatriz.java
+++ 200-exercicios-java/src/main/java/br/com/capitulo5/CriacaoMatriz.java
@@ -26,7 +26,7 @@ public class CriacaoMatriz {
 		System.out.println("matriz 3x3");
 		for(int i = 0;i < 3;i++) {
 			for(int j = 0;j < 3; j++) {
-				System.out.println(matriz[i][j]+" ");
+				System.out.print(matriz[i][j]+" ");
 			}
 			System.err.println();
 		}
