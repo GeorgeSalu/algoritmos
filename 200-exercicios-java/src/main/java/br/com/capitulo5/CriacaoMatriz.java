f06d9b6329539be3867dd182f2ba754f3741b8e8
George Salu <george.salu10@gmail.com>
2025-03-24 21:28:51 -0300

    200-exercicios-java - arrays e colecoes - exercicio criacao de uma matriz

--- 200-exercicios-java/src/main/java/br/com/capitulo5/CriacaoMatriz.java
+++ 200-exercicios-java/src/main/java/br/com/capitulo5/CriacaoMatriz.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * 
  * 
  * */
-public class CriacaoMariz {
+public class CriacaoMatriz {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
