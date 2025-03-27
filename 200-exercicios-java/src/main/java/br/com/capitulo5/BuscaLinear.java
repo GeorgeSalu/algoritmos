d734b63b9df489efce06f1c402480824c97c0337
George Salu <george.salu10@gmail.com>
2025-03-27 11:52:16 -0300

    200-exercicios-java - manipulacao de arrays - exercicio busca linear em um array

--- 200-exercicios-java/src/main/java/br/com/capitulo5/BuscaLinear.java
+++ 200-exercicios-java/src/main/java/br/com/capitulo5/BuscaLinear.java
@@ -1,14 +1,41 @@
 package br.com.capitulo5;
 
+import java.util.Scanner;
+
 /*
  * 
- * Enunciado: Crie um programa que leia 10 números inteiros e um número adicional. O programa deve realizar uma busca 
+ * Crie um programa que leia 10 números inteiros e um número adicional. O programa deve realizar uma busca 
  * linear no array para verificar se o número adicional está presente. Exiba a posição do número, se encontrado.
  * 
  * 
  * */
 public class BuscaLinear {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
+		int numeros[] = new int[10];
+		boolean encontrado = false;
+		
+		for(int i = 0;i < numeros.length; i++) {
+			System.out.println("digite o numero "+(i+1)+" : ");
+			numeros[i] = scanner.nextInt();
+		}
+		
+		System.out.println("digite o numero que deseja buscar : ");
+		int numeroBuscado = scanner.nextInt();
+		
+		for(int i = 0;i < numeros.length; i++) {
+			if(numeros[i] == numeroBuscado) {
+				System.out.println("numero encontrado na posicao : "+i);
+				encontrado = true;
+				break;
+			}
+		}
+		
+		if(!encontrado) {
+			System.out.println("numero encontrado");
+		}
+		
+		scanner.close();
 	}
 }
