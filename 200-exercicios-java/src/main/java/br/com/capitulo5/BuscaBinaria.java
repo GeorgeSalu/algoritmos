498cd4fb19ac612b959ff400ffdc7f3649a7759d
George Salu <george.salu10@gmail.com>
2025-03-27 12:34:30 -0300

    200-exercicios-java - manipulacao de arrays - exercicio busca binaria em um array ordenado

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo5/BuscaBinaria.java
@@ -0,0 +1,40 @@
+package br.com.capitulo5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+ * 
+ * Desenvolva um programa que leia 10 números inteiros, os ordene em ordem crescente e, em seguida, utilize o método de busca 
+ * binária para encontrar um número fornecido pelo usuário. Exiba a posição do número se ele for encontrado.
+ * 
+ * 
+ * */
+public class BuscaBinaria {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int numeros[] = new int[10];
+		
+		for(int i = 0;i < numeros.length; i++) {
+			System.out.println("digite o numero "+(i+1)+" :");
+			numeros[i] = scanner.nextInt();
+		}
+		
+		Arrays.sort(numeros);
+		
+		System.out.println("digite o numero que deseja buscar : ");
+		int numeroBuscado = scanner.nextInt();
+		
+		// realiza a buca binaria
+		int posicao = Arrays.binarySearch(numeros, numeroBuscado);
+		
+		if(posicao >= 0) {
+			System.out.println("numero encontrado na posicao "+posicao);
+		} else {
+			System.out.println("numero nao encontrado");
+		}
+		
+		scanner.close();
+	}
+}
