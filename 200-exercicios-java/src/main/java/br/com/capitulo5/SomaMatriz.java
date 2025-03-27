527e3a756d462d38a0815839c3d2940c70a60705
George Salu <george.salu10@gmail.com>
2025-03-26 22:07:31 -0300

    200-exercicios-java - arrays e colecoes - exercicio soma de duas matrizes

---
+++ 200-exercicios-java/src/main/java/br/com/capitulo5/SomaMatriz.java
@@ -0,0 +1,51 @@
+package br.com.capitulo5;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * Enunciado:  Escreva um programa que leia duas matrizes 3x3 e calcule a soma entre elas, exibindo a matriz resultante.
+ * 
+ * 
+ * */
+public class SomaMatriz {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int matrizA[][] = new int[2][2];
+		int matrizB[][] = new int[2][2];
+		int soma[][] = new int[2][2];
+		
+		System.out.println("preenchendo a matriz A : ");
+		for(int i = 0;i < 2; i++) {
+			for(int j = 0;j < 2; j++) {
+				System.out.println("digite o valor para a posicao ["+i+"]["+j+"]");
+				matrizA[i][j] = scanner.nextInt();
+			}
+		}
+		
+		System.out.println("preenchendo a matriz B : ");
+		for(int i = 0;i < 2; i++) {
+			for(int j = 0;j < 2; j++) {
+				System.out.println("digite o valor para a posicao ["+i+"]["+j+"]");
+				matrizB[i][j] = scanner.nextInt();
+			}
+		}
+		
+		for(int i = 0;i < 2; i++) {
+			for(int j = 0;j < 2; j++) {
+				soma[i][j] = matrizA[i][j] + matrizB[i][j];
+			}
+		}
+		
+		System.out.println("soma das matrizes A e B : ");
+		for(int i = 0;i < 2; i++) {
+			for(int j = 0;j < 2; j++) {
+				System.out.println(soma[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+		scanner.close();
+	}
+}
